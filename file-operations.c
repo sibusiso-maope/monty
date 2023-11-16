b64c74271b31cf2a58dc6e02baf7b8dd7824217f
sibusiso-maope <sibusiso.maope@gmail.com>
2023-11-16 20:38:08 +0200

    some fixes

--- file-operations.c
+++ file-operations.c
@@ -1,23 +1,5 @@
 #include "monty.h"
 
-/**
- * read_file - reads a file
- * @fd: pointer to file descriptor
- * Return: void
- */
-
-void read_file(FILE *fd)
-{
-	int line_number, format = 0;
-	char *buffer = NULL;
-	size_t length = 0;
-
-	for (line_number = 1; getline(&buffer, &length, fd) != -1; line_number++)
-	{
-		format = parse_line(buffer, line_number, format);
-	}
-	free(buffer);
-}
 
 /**
  * open_file - opens a file
